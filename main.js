f62fd374edfbdc429bc64ff7d58ca1b1a22ab954
Yggdrasil09 <karthik99th.tnk@gmail.com>
2019-11-20 12:07:52 +0530

    Saving files added

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const { app, BrowserWindow } = require("electron");
-const { ipcMain } = require('electron');
-const Automerge = require('automerge')
+const { ipcMain } = require("electron");
+const Automerge = require("automerge");
+const fs = require('fs');
+const server = require('http').createServer();
+const io = require('socket.io')(server)
 
-let doc1 = Automerge.from({ cards: [] })
+let doc1 = Automerge.from({ cards: [] });
 
 let win;
 
@@ -13,7 +16,7 @@ function createWindow() {
     }
   });
 
-  win.maximize()
+  win.maximize();
 
   win.loadFile("src/index.html");
 
@@ -36,6 +39,30 @@ app.on("activate", () => {
   }
 });
 
-ipcMain.on('live-editing',(event,arg) => {
+ipcMain.on("live-editing", (event, arg) => {
   console.log(arg);
 });
+
+// ipcMain.on("save-file", (event, arg) => {});
+
+io.on('connection',function(client){
+
+  client.on('share',()=>{
+    console.log("connected")
+  })
+
+  client.on('disconnect',function(){
+    console.log('client disconnected ..',client.id)
+  })
+
+  client.on('error',function(err){
+    console.log('received error from client : ',client.id)
+    console.log(err)
+  })
+
+})
+
+server.listen(3000,function(err){
+  if (err) throw err
+  console.log('listening on port : 3000')
+})
--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 const { ipcRenderer } = require("electron");
+const prompt = require('electron-prompt');
 
 var tabs = [];
 let tab_bars = document.getElementById("tab-bars");
@@ -6,41 +7,45 @@ let code_editors = [];
 let body_area = document.getElementById("content-area");
 
 window.onkeyup = function(e) {
-  if (e.ctrlKey && e.which == 78) {
 
+	// New tab
+  if (e.ctrlKey && e.which == 78) {
     let tag = document.createElement("li");
-	let link = document.createElement("a");
-	
+    let link = document.createElement("a");
+
+    $(tag).attr("id", "tab" + (tabs.length + 1).toString());
+
     $(link)
       .attr("href", "#menu" + (tabs.length + 1).toString())
-	  .attr("data-toggle", "tab");
-	  
+      .attr("data-toggle", "tab").attr("id", "link"+(tabs.length + 1).toString());
+
     link.innerHTML =
-      "Untitled Tab " +
-      '<i class="fa fa-close" id="close' +
+      "Untitled Tab" +
+      ' <i class="fa fa-close" id="close' +
       (tabs.length + 1).toString() +
       '" style="font-size:18px;cursor:pointer" ></i>';
+
     if (tabs.length == 0) {
       $(tag).addClass("active");
-	}
-	
+    }
+
     $(tag).append(link);
-	$(tab_bars).append(tag);
-	
+    $(tab_bars).append(tag);
+
     let div = document.createElement("div");
     let label = document.createElement("label");
-	let text_area = document.createElement("textarea");
-	
+    let text_area = document.createElement("textarea");
+
     $(div)
       .addClass("tab-pane fade in active")
       .attr("id", "menu" + (tabs.length + 1).toString());
     $(text_area).attr("id", "code" + (tabs.length + 1).toString());
     $(label).append(text_area);
     $(div).append(label);
-	$(body_area).append(div);
-	
-	tabs.push(["Untitled Tab", "default", tabs.length + 1,tag]);
-	
+    $(body_area).append(div);
+
+    tabs.push(["Untitled Tab", "default", tabs.length + 1]);
+
     let textarea = document.getElementById("code" + tabs.length.toString());
     let editor = CodeMirror.fromTextArea(textarea, {
       lineNumbers: true,
@@ -50,20 +55,80 @@ window.onkeyup = function(e) {
       gutters: ["CodeMirror-lint-markers"],
       styleActiveLine: true,
       lint: true
-	});
-	
+    });
+
     code_editors.push(editor);
   }
 
-  for (let i = 0; i < tabs.length; i++) {
-    document.getElementById("close"+tabs[i][2].toString()).addEventListener('click',()=>{
-		document.getElementById("menu"+tabs[i][2].toString()).remove()
-		tabs.splice(i,1);
-		code_editors.splice(i,1);
-	})
-  }
+  //file saving
+
+  if(e.ctrlKey && e.which == 83)
+	{
+		for(let i=0;i<tabs.length;i++)
+		{
+			if($(document.getElementById("tab" + tabs[i][2].toString())).hasClass('active'))
+			{
+				let filename = tabs[i][0];
+				if(filename == "Untitled Tab")
+				{
+          prompt({
+            title: 'Save file',
+            label: 'Enter the name of file:',
+            value: '',
+            inputAttrs: {
+                type: 'text'
+            },
+            type: 'input'
+          })
+          .then((r) => {
+              if(r === null) {
+                  console.log('user cancelled');
+              } 
+              else
+              {
+                filename = r;
+                tabs[i][0] = filename;
+                let link = document.getElementById("link"+tabs[i][2].toString());
+                link.innerHTML =filename +
+                ' <i class="fa fa-close" id="close' +
+                (tabs[i][2]).toString() +
+                '" style="font-size:18px;cursor:pointer" ></i>';
+              }
+          })
+          .catch(console.error);
+        }
+
+				let text = code_editors[i].getValue();
+				ipcRenderer.send("save-file", text);
+			}
+		}
+	}
 
 };
+
+setInterval(() => {
+  for (let i = 0; i < tabs.length; i++) {
+    if (!document.getElementById("close" + tabs[i][2].toString())) continue;
+    document
+      .getElementById("close" + tabs[i][2].toString())
+      .addEventListener("click", () => {
+        if (document.getElementById("menu" + tabs[i][2].toString()))
+          document.getElementById("menu" + tabs[i][2].toString()).remove();
+        if (document.getElementById("tab" + tabs[i][2].toString()))
+          document.getElementById("tab" + tabs[i][2].toString()).remove();
+        tabs.splice(i, 0);
+        code_editors.splice(i, 0);
+        if (tabs.length != 0) {
+          $(document.getElementById("tab" + tabs[0][2].toString())).addClass(
+            "active"
+          );
+          $(document.getElementById("menu" + tabs[0][2].toString())).addClass(
+            "active"
+		  );
+        }
+      });
+  }
+}, 500);
 
 // let work = setInterval(() => {
 //   for (let i = 0; i < code_editors.length; i++) {
